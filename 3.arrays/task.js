db7a98abeb0d63d0dc30fb2a454d94c2228467af
pevec1 <pevec1@yandex.ru>
2023-04-07 21:55:31 +0300

    решил первую задачу

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,16 @@
+
+
 function compareArrays(arr1, arr2) {
-  
+        let l = false;
+        if (arr1.length === arr2.length) {
+            l = true;
+        } else if(arr1.length < arr2.length) {
+            l = false;
+        }else if(arr1.length > arr2.length) {
+            l = false;
+        }
+
+        return l && arr1.every((elem, index) => elem == arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
