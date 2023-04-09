caf1457b5945bca0373fe65eb32aa7ee45a0aa88
pevec1 <pevec1@yandex.ru>
2023-04-09 22:50:45 +0300

    сделал объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,43 @@
 function Student(name, gender, age) {
-  
+    this.name = name,
+    this.gender = gender,
+    this.age = age,
+    this.marks = []
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+let student2 = new Student("Carina", "female", 25);
+let student3 = new Student("Andrei", "male", 30);
+
+Student.prototype.setSubject = function(subjectName) {
+    return this.subject = subjectName
+}
+let student = new Student("Petya", "male", 20);
+student.setSubject("A");
+
+Student.prototype.addMarks = function(...marks) {
+    if (this.marks !== undefined) {
+        return this.marks.push(...marks)
+    }
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.getAverage = function() {
+    let sum = 0;
+    let l = 0;
+    if (this.marks !== undefined) {
+        l = this.marks.length
+    }
+    if (l === 0) {
+        return this.average = 0
+    }
+    for (let i = 0; i < l; i++) {
+        sum = sum + Number(this.marks[i]);
+    }
+    return this.average = sum / l
 }
 
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+    delete this.subject
+    delete this.marks
+    delete this.average
+    return this.excluded = reason
 }
