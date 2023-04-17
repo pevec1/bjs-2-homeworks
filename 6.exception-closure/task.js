b4f78e420b51553e1baec830aa34030e55d1954a
pevec1 <pevec1@yandex.ru>
2023-04-17 19:51:20 +0300

    update

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,17 @@
-﻿
+﻿function parseCount (changeNumber) {
+  let parse = Number.parseFloat(changeNumber)
+  if (Number.isNaN(parse)) {
+    throw new Error('Невалидное значение')
+  }
+  return parse
+}
+
+function validateCount (changeNumber) {
+  let parse
+  try {
+    parse = parseCount(changeNumber)
+    return parse
+  } catch (error) {
+    return error
+  }
+}
