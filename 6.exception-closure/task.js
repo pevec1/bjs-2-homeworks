a01e2e93815a9c18ccac934bd2fd35ff87b0e454
pevec1 <pevec1@yandex.ru>
2023-04-18 22:57:56 +0300

    Revert "обнова"
    This reverts commit c99519b52fcc1e405105539a009590d77e69f34c.

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -15,27 +15,3 @@ function validateCount (changeNumber) {
     return error
   }
 }
-
-//exe2
-
-class Triangle{
-    constructor(a, b, c){
-        if (a+b<=c || a+c<=b || b+c<=a){
-            throw new Error('Треугольник с такими сторонами не существует');
-        }
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-    get perimeter(){
-        return this.a+this.b+this.c;
-    }
-    get area(){
-        let p = this.perimeter/ 2;
-        let area = Math.sqrt(p*(p-this.a)*(p-this.b)*(p-this.c));
-        return parseFloat(area.toFixed(3));
-    }
-   
-}
-// let trio = new Triangle(2,5,5)
-// alert (trio.perimeter)
