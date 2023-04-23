425700cb395e1dbd689da27eb1354b11e7fc75c3
pevec1 <pevec1@yandex.ru>
2023-04-23 23:18:08 +0300

    решение задачи

---
+++ 7.async/task.js
@@ -0,0 +1,57 @@
+class AlarmClock {
+  constructor () {
+    this.alarmCollection = []
+    this.intervalId = null
+  }
+  addClock (time, clock) {
+    if (!time || !clock) {
+      throw new Error('Отсутствуют обязательные аргументы')
+    }
+    if (this.alarmCollection.filter((value, index, arr) => value === clock)) {
+      console.warn('Уже присутствует звонок на это же время')
+    }
+    let obj = { callback: clock, time: time, canCall: true }
+    this.alarmCollection.push(obj)
+  }
+  removeClock (time) {
+    const arr = this.alarmCollection.filter(
+      (value, index, arr) => value.time !== time
+    )
+    this.alarmCollection = arr
+  }
+  getCurrentFormattedTime () {
+    return new Date().toLocaleTimeString('ru-Ru', {
+      hour: '2-digit',
+      minute: '2-digit'
+    })
+  }
+  start () {
+    if (this.intervalId) {
+      return
+    }
+    this.intervalId = setInterval(() => {
+      this.alarmCollection.forEach((item, index, array) => {
+        if (
+          item.time === this.getCurrentFormattedTime() &&
+          item.canCall === true
+        ) {
+          item.canCall = false
+          item.callback()
+        }
+      })
+    }, 1000)
+  }
+  stop () {
+    clearInterval(this.intervalId)
+    this.intervalId = null
+  }
+  resetAllCalls () {
+    this.alarmCollection.forEach((item, index, array) => {
+      item.canCall = true
+    })
+  }
+  clearAlarms () {
+    this.stop()
+    this.alarmCollection = []
+  }
+}
