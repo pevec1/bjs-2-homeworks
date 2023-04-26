5a85ea9f9af1aa031290d01a442950b2c70cd68f
pevec1 <pevec1@yandex.ru>
2023-04-26 08:26:41 +0300

    решена задача 1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,6 +1,26 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = md5(args); // получаем правильный хеш c помощью функции md5
+        let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
+        let index = cache.findIndex(arr => arr.hash === hash);
+        if (objectInCache) { // если элемент найден
+            alert(JSON.stringify(cache), index)
+            console.log(`Из кэша: ${cache[index].result}`); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+            return `Из кэша: ${cache[index].result}`;
+        }
+    
+        let result = func(...args); // в кеше результата нет — придётся считать
+        cache.push({hash, result}) ; // добавляем элемент с правильной структурой
+        if (cache.length > 5) { 
+          cache.shift(); // если слишком много элементов в кеше, надо удалить самый старый (первый) 
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;  
+    }
+    return wrapper;
 }
 
 //Задача № 2
